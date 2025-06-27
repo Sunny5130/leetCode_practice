aabb1d61d99fa497884726235eaf959cf6d61119
Sunny saini <115605667+Sunny5130@users.noreply.github.com>
2025-06-27 13:50:29 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (51.96%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        //kadanes's algorithm
         int sum=0;
-        int maxi=nums[0];
+        int maxi=INT_MIN;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            maxi=max(maxi,sum);
+            if(sum>maxi){
+                maxi=sum;
+            }
             if(sum<0){
                 sum=0;
             }
