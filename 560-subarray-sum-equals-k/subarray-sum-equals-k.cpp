be0ca59ed7f2ca3d4cc7da2ea6f309b747f294ce
Sunny saini <115605667+Sunny5130@users.noreply.github.com>
2025-07-01 17:32:10 +0530

    Time: 2509 ms (9.07%) | Memory: 34.3 MB (94.02%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,19 +1,16 @@
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
-    int subarraySum(vector<int>& arr, int k) {
-        unordered_map<int, int> pref;
-        pref[0] = 1; 
-        int count = 0, sum = 0;
-        
-        for (int i=0;i<arr.size();i++) {
-            sum += arr[i];
-            if (pref.find(sum - k) != pref.end()) {
-                count += pref[sum - k];
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                if(sum==k){
+                    count++;
+                }
             }
-            pref[sum]++;
         }
         return count;
     }
