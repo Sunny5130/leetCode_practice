ef2b51969810809149af6eae8a7c5522707f1eb4
Sunny saini <115605667+Sunny5130@users.noreply.github.com>
2025-07-17 22:43:37 +0530

    Time: 4 ms (17.98%) | Memory: 32.8 MB (43.99%) - LeetSync

---
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        vector<int>result;
+        int i= num.size()-1;
+        int carry=k;
+        while(i>=0 || carry>0) {
+            if (i >= 0)carry+=num[i--];
+            result.push_back(carry%10);
+            carry/=10;
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
