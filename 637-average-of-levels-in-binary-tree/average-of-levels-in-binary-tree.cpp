4fdb4ec8b86feed6aa3bdb30b5133d60d1cc7917
Sunny saini <115605667+Sunny5130@users.noreply.github.com>
2025-08-19 22:28:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (53.26%) - LeetSync

---
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double>ans;
+        if(root==NULL)return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int s=q.size();
+            // vector<int>level;
+            double av=0;
+            double count=0;
+            for(int i=0;i<s;i++){
+                TreeNode* temp=q.front();
+                q.pop();
+                av+=temp->val;
+                count++;
+                if(temp->left){
+                    q.push(temp->left);
+                }
+                if(temp->right){
+                    q.push(temp->right);
+                }
+            }
+            ans.push_back(av/count);
+        }
+        return ans;
+    }
+};
