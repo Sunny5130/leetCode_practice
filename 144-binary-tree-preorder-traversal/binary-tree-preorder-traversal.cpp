0e0a6fa5546ca7f11b5dbf1072e625e571df25c7
Sunny saini <115605667+Sunny5130@users.noreply.github.com>
2025-08-19 23:23:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (13.82%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -6,23 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>preorder;
-        if(root==NULL) return preorder;
-        stack<TreeNode*> st;
+        vector<int> preorder;
+        if (root == NULL)
+        return preorder;
+        stack<TreeNode*> st;     //same level order but use stack instead of queue
         st.push(root);
-        while(!st.empty()){
-            root=st.top();
+        while (!st.empty()) {
+            root = st.top();
             st.pop();
             preorder.push_back(root->val);
-            if(root->right!=NULL){
+            if (root->right != NULL) {
                 st.push(root->right);
-            }if(root->left!=NULL){
+            }
+            if (root->left != NULL) {
                 st.push(root->left);
             }
         }
