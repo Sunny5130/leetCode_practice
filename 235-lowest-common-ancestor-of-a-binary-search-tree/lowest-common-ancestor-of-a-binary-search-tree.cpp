c0edc6641aa1c59d60e328153c91183c498cde8c
Sunny saini <115605667+Sunny5130@users.noreply.github.com>
2025-08-24 17:42:58 +0530

    Time: 23 ms (53.32%) | Memory: 23.5 MB (32.68%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,14 +11,14 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL) return NULL;
-        int curr=root->val;
-        if(curr < p->val && curr < q->val){
-            return lowestCommonAncestor(root->right,p,q);
-        }
-        if(curr > p->val && curr > q->val){
+        if(root==NULL)return NULL;
+        if(root->val > p->val && root->val > q->val){
             return lowestCommonAncestor(root->left,p,q);
         }
-        return root;
+        if(root->val < p->val && root->val < q->val){
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        return root;//if p and q dono root ke left or right subtree mai ho means 
+                    //dono ek taraf na ho toh
     }
 };
