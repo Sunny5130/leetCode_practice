cf9ac4ec84b39f4b964372208bac9086358be9ff
Sunny saini <115605667+Sunny5130@users.noreply.github.com>
2025-08-28 22:23:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (67.54%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
-        long long no=x;
+        if(x<0)return false;
+        long long orig=x;
         long long ans=0;
-        while(x>0){
-            ans=(ans*10)+(x%10);
+        while(x > 0){
+            ans =ans*10+(x%10);
             x=x/10;
         }
-        return(ans == no);
+        return orig==ans;
     }
 };
