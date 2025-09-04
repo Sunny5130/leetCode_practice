369b8e0fbcb638e6dbbfd998db5db1f3f8ba3862
Sunny saini <115605667+Sunny5130@users.noreply.github.com>
2025-09-04 23:20:26 +0530

    Time: 39 ms (66.41%) | Memory: 85.9 MB (47.92%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -1,31 +1,17 @@
-// class Solution {
-// public:
-//     int maximumGap(vector<int>& nums) {
-//         int n=nums.size();
-//         if(nums.size()<2)return 0;
-//         sort(nums.begin(),nums.end());
-//         int diff=nums[0];
-//         bool flag=true;
-//         for(int i=1;i<nums.size();i++){
-//             if(nums[i]==nums[i-1])flag=false;
-//             int count = (nums[i]-nums[i-1]);
-//             diff=max(count,diff);
-//         }
-//         if(flag==false && nums[0]==nums[n-1])return 0;
-//         return diff;
-//     }
-// };
-
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        int n = nums.size();
-        if (n < 2) return 0;
-        sort(nums.begin(), nums.end());
-        int diff = 0; 
-        for (int i = 1; i < n; i++) {
-            diff = max(diff, nums[i] - nums[i-1]);
+        int n=nums.size();
+        if(nums.size()<2)return 0;
+        sort(nums.begin(),nums.end());
+        int diff=0;
+        bool flag=true;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1])flag=false;
+            int count = (nums[i]-nums[i-1]);
+            diff=max(count,diff);
         }
+        if(flag==false && nums[0]==nums[n-1])return 0;
         return diff;
     }
 };
