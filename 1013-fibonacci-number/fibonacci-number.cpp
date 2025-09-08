27b585deb6a1ba3f80780df297ab963f59b93a6b
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-09-08 10:38:52 +0530

    Time: 14 ms (22.11%) | Memory: 7.7 MB (63.89%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 )return 0;
-        if(n==1)return 1; 
-        int a=0;
-        int b=1;
-        int res;
-        for(int i=2;i<=n;i++){
-            res=a+b;
-            a=b;
-            b=res;
-        }
-        return res;
+    //----------------------------iterative------------------//
+        // if(n==0 )return 0;
+        // if(n==1)return 1; 
+        // int a=0;
+        // int b=1;
+        // int res;
+        // for(int i=2;i<=n;i++){
+        //     res=a+b;
+        //     a=b;
+        //     b=res;
+        // }
+        // return res;
+    //---------------------------recursion-------------------//
+    if(n==0 ||n==1)return n;
+    return fib(n-1) + fib(n-2);
     }
 };
