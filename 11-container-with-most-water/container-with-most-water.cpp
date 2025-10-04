1985fbca14712a1669d67cefc6f07f20ad8bfa4c
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-10-04 18:14:03 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (12.56%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int maxArea(vector<int>& h) {
-        int l=0;
-        int r=h.size()-1;
-        int area=0;
-        while(l<r){
-        int currentarea=min(h[l], h[r]) * (r-l);
-        area=max(area,currentarea);
-        if(h[l]>h[r]){
-            r--;
-        }else{
+//------------------------====brute froce--
+        // int mw=0;   
+        // for(int i=0;i<h.size();i++){
+        //     for(int j=i+1;j<h.size();j++){
+        //         int w=j-i;
+        //         int height=min(h[i],h[j]);
+        //         int area=w*height;
+        //         mw=max(mw,area);
+        //     }
+        // }
+        // return mw;
+//-----------------------------optimize using two pointer approach----------------------//
+    int l=0;
+    int r=h.size()-1;
+    int mw=0;
+    while(l<r){
+        int area=(min(h[l],h[r]))*(r-l);
+        mw=max(area,mw);
+        if(h[l]<h[r]){
             l++;
+        }else{
+            r--;
         }
-        }
-        return area;
+    }
+    return mw;
     }
 };
-
-
-
-
-
