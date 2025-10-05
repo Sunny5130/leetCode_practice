a8f6213ced31bedd48b45010a9aa39d65994d949
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-10-05 22:06:54 +0530

    Time: 0 ms (100.00%) | Memory: 62.9 MB (48.21%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int maxArea(vector<int>& h) {
+    int maxArea(vector<int>&h){
 //------------------------====brute froce--
         // int mw=0;   
         // for(int i=0;i<h.size();i++){
@@ -16,7 +16,7 @@ public:
     int l=0;
     int r=h.size()-1;
     int mw=0;
-    while(l<r){
+    while(l<=r){
         int area=(min(h[l],h[r]))*(r-l);
         mw=max(area,mw);
         if(h[l]<h[r]){
