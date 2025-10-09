f368cf520de7635e0ab695ed2b87e5090d1fd0f4
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-10-09 19:13:32 +0530

    Time: 3 ms (69.51%) | Memory: 14.9 MB (36.09%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=i;j<nums.size();j++){
-                if(i==j)continue;
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                }
+        // for(int i=0;i<nums.size();i++){    //use two loops
+        //     for(int j=i;j<nums.size();j++){
+        //         if(i==j)continue;
+        //         if(nums[i]+nums[j]==target){
+        //             return {i,j};
+        //         }
+        //     }                //time complexity o(n^2)
+        // }
+        // return {};
+
+    unordered_map<int,int> Map;
+        for (int i =0;i<nums.size();i++) {
+            int complement = target - nums[i];
+            if (Map.count(complement)) {            //time complexity  O(n)
+                return {Map[complement],i};
             }
+            Map[nums[i]] = i;
         }
         return {};
+
+
 
     //     sort(nums.begin(),nums.end()); //yei code pair hai two element ka sum target ke equal
     //     int i=0,j=nums.size()-1;
@@ -22,7 +34,7 @@ public:
     //         }else{
     //             j--;
     //         }
-    //     }
+    //     }                                        //T.C=  O(N(lon(N)))
     //     return {};
     }
 };
