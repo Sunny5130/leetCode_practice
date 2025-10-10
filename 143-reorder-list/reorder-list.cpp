52cd0c70b8142343b255f2865219449dcf8ddee3
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-10-10 19:27:49 +0530

    Time: 3 ms (14.75%) | Memory: 24 MB (7.89%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(!head || !head->next) return;
+        vector<ListNode*> nodes;
+        ListNode* temp = head;
+        while(temp) {
+            nodes.push_back(temp);
+            temp = temp->next;
+        }
+        int i =0,j=nodes.size()-1;
+        while(i<j){
+            nodes[i++]->next = nodes[j];
+            if(i==j)break;
+            nodes[j--]->next=nodes[i];
+        }
+        nodes[i]->next = nullptr;
+    }
+};
