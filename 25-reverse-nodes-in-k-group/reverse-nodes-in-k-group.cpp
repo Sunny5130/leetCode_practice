fbee45468ab875e2b22b7a485131532c8b0f789a
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-10-10 21:52:18 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (5.24%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -11,29 +11,46 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head==NULL || k==1)return head;
-        int count=0;
-        ListNode*temp=head;
-        while(temp!=NULL &&count<k){
+    //     if(head==NULL || k==1)return head;
+    //     int count=0;
+    //     ListNode*temp=head;
+    //     while(temp!=NULL &&count<k){
+    //         temp=temp->next;
+    //         count++;
+    //     }
+    //     if(count<k)return head;
+    //     ListNode*prev=NULL;
+    //     ListNode*curr=head;
+    //     ListNode*nex;
+    //     count=0;
+    //     while(curr!=NULL && count < k){
+    //         nex=curr->next;
+    //         curr->next=prev;
+    //         prev=curr;
+    //         curr=nex;
+    //         count++;
+    //     }
+    //     if(nex!=NULL){
+    //         head->next=reverseKGroup(nex,k);
+    //     }
+    //     return prev;
+    // }
+    vector<int>ans;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            ans.push_back(temp->val);
             temp=temp->next;
-            count++;
         }
-        if(count<k)return head;
-
-        ListNode*prev=NULL;
-        ListNode*curr=head;
-        ListNode*nex;
-        count=0;
-        while(curr!=NULL && count < k){
-            nex=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=nex;
-            count++;
+        for(int i=0;i+k<=ans.size();i+=k){
+            reverse(ans.begin()+i,ans.begin()+i+k);
         }
-        if(nex!=NULL){
-            head->next=reverseKGroup(nex,k);
+        ListNode* dummy=new ListNode(0);
+        temp=dummy;
+        for(int i=0;i<ans.size();i++){
+            ListNode* node=new ListNode(ans[i]);
+            temp->next=node;
+            temp=temp->next;
         }
-        return prev;
+        return dummy->next;
     }
 };
