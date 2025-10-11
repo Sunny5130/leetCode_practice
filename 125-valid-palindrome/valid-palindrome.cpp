311ca3c3c468d16171a6d61d59432e4e97d78422
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-10-11 17:21:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (6.15%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,14 +1,15 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string f;
-                for (char c : s) {
-            if (isalnum(c)) {
-                f += tolower(c);
-            }
+        string res="";
+        for(int i=0;i<s.length();i++){
+            if(isalnum(s[i])){
+                res+=tolower(s[i]);
         }
-        string reversedStr = f;
-        reverse(reversedStr.begin(), reversedStr.end());
-        return f == reversedStr;
+    }
+        string ans=res;
+        reverse(res.begin(),res.end());
+        return res==ans;
     }
 };
