bc0e7179773a035538f7b967ba3624d36ed40744
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-11-17 18:37:43 +0530

    Time: 14 ms (6.32%) | Memory: 11.1 MB (98.55%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -15,12 +15,11 @@ public:
             fast=fast->next->next;
             slow=slow->next;
             if(slow==fast){
-                slow=head;
-                while(slow!=fast){
-                    slow=slow->next;
-                    fast=fast->next;
-                }
-                return slow;
+               fast=head;
+               while(slow!=fast){
+                slow=slow->next;
+                fast=fast->next;
+               }return slow;
             }
         }
 return NULL;
