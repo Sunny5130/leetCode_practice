ff77bdad9786ef67de3e6f6f29915ed72e9ff33c
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-11-17 17:40:26 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (48.10%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -14,19 +14,15 @@ public:
         // return mw;
 //-----------------------------optimize using two pointer approach----------------------//
     
-    int l=0;
-    int r=h.size()-1;
-    int mw=0;
-    while(l<=r){
-        int area=(min(h[l],h[r]))*(r-l);
-        mw=max(area,mw);
-        if(h[l]<h[r]){
-            l++;
-        }else{
-            r--;
-        }
+    int l=0,r=h.size()-1;
+    int maxa=0;
+    while(l<r){
+        int area=(min(h[l],h[r])*(r-l));
+        maxa=max(area,maxa);
+        if(h[l]>h[r])r--;
+        else l++;
     }
-    return mw;
+    return maxa;
     }
 };
 
