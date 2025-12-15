76ee33b64fd0e57a7648e084da6dccd05f404d17
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-12-15 23:00:25 +0530

    Time: 61 ms (12.88%) | Memory: 23.9 MB (10.36%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,42 +9,36 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        // ListNode*temp1=headA;
-        // ListNode*temp2=headB;
-        // int c1=0;
-        // int c2=0;
-        // while(temp1!=NULL){
-        //     c1++;
-        //     temp1=temp1->next;
-        // }
-        // while(temp2!=NULL){
-        //     c2++;
-        //     temp2=temp2->next;
-        // }
-        // int n=abs(c1-c2);
-        // temp1=headA;
-        // temp2=headB;
-        // if(c1>c2){
-        //     while(n--) temp1 = temp1->next;
-        // }else{
-        //     while(n--) temp2 = temp2->next;
-        // }
-        //  while(temp1 != NULL && temp2 != NULL) {
-        //     if(temp1 == temp2) return temp1;
-        //     temp1 = temp1->next;
-        //     temp2 = temp2->next;
-        // }
-        // return NULL;
-
-        //---------------------------By---------------------------------//
-        ListNode* h1=headA;
-        ListNode* h2=headB;
-        while(h1!=h2){
-            if(h1==NULL)h1=headB;
-            else h1=h1->next;
-            if(h2==NULL)h2=headA;
-            else h2=h2->next;
+        int l1=1,l2=1;
+        ListNode* temp=headA;
+        while(temp!=NULL){
+            l1++;
+            temp=temp->next;
         }
-        return h1;
+        temp=headB;
+        while(temp!=NULL){
+            temp=temp->next;
+            l2++;
+        }
+        ListNode* a=headA;
+        ListNode* b=headB;
+        int diff=abs(l1-l2);
+        if(l1-l2>0){
+            while(diff){
+                a=a->next;
+                diff--;
+            }
+        }else{
+            while(diff){
+                b=b->next;
+                diff--;
+            }
+        }
+        while(a!=NULL && b!=NULL){
+            if(a==b)return a;
+            a=a->next;
+            b=b->next;
+        }
+        return 0;
     }
 };
