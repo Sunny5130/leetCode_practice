6520b35a9c266c13fbd9f94459f5d79ebd857d3b
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-12-16 15:49:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (20.36%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.length()%2!=0)return false;
+        // if(s.length()%2!=0)return false;
         stack<char>st;
         for(int i=0;i<s.length();i++){
             if(s[i]=='(' || s[i]=='{'||s[i]=='['){
