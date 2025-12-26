6498389725e8372e1f09712e3372c257779c70fb
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-12-26 14:51:02 +0530

    Time: 2 ms (4.77%) | Memory: 8.7 MB (16.44%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==1 || n==0)return 1;
+        // if(n==1 || n==0)return 1;
         vector<int>dp(n+1,0);
         dp[0]=dp[1]=1;
         for(int i=2;i<=n;i++){
@@ -10,3 +10,4 @@ public:
         return dp[n];
     }
 };
+
