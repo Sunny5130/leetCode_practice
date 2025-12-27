70704bea144819337fcaacce28d046b8c473a19d
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-12-27 14:24:48 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (36.54%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -1,25 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
- #include<bits/stdc++.h>
+// class Solution {
+// public:
+//     int getDecimalValue(ListNode* head) {
+//     ListNode* temp=head;
+//     string ans="";
+//     while(temp!=NULL){
+//         ans+=(temp->val +'0');
+//         temp=temp->next;
+//     }
+//     int s=stoi(ans,nullptr,2);
+//     return s;        
+//     }
+// };
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
-        string s="";
+        int sum= 0;
         ListNode* temp=head;
-        while(temp!=NULL){
-            s += (temp->val+'0');
+        while (temp!=NULL) {
+            sum=sum*2 + temp->val;
             temp=temp->next;
         }
-    int d=stoi(s,nullptr,2); 
-        return d;
+        return sum;
     }
 };
-
