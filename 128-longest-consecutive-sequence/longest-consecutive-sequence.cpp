918ff239479f973e2b621f8d5df46e1c64b0db35
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-12-30 16:15:17 +0530

    Time: 84 ms (51.52%) | Memory: 88.8 MB (74.19%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -17,3 +17,7 @@ public:
         return maxi;
     }
 };
+//set ke ander element daal denge then set ko traverse karenge or check karenge ki
+//agar set ka element ka ek kamm elemnt hai toh chod denge kyoki wahe sequence peache se chalti aa rahi hai
+//agr element -1 set mai nhai hai toh matlab sequence idhar se suru hai or ellement ka +1 karenge or set mai check 
+// karenge agar hai tob usko count++ kardo
