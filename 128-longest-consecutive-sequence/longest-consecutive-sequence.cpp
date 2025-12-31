4ca04e0a735629372a45051909bf8b3ec182844d
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-12-31 23:16:56 +0530

    Time: 75 ms (77.43%) | Memory: 88.9 MB (40.78%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int>set(nums.begin(),nums.end());
         int maxi=0;
-        for(auto it : set){
-            if(!set.count(it-1)){
+        unordered_set<int>st(nums.begin(),nums.end());
+        for(auto it:st){
+            if(!st.count(it-1)){
                 int current=it;
                 int count=0;
-                while(set.count(current)){
-                    count++;
+                while(st.count(current)){
                     current++;
+                    count++;
                 }
                 maxi=max(count,maxi);
             }
@@ -17,7 +17,3 @@ public:
         return maxi;
     }
 };
-//set ke ander element daal denge then set ko traverse karenge or check karenge ki
-//agar set ka element ka ek kamm elemnt hai toh chod denge kyoki wahe sequence peache se chalti aa rahi hai
-//agr element -1 set mai nhai hai toh matlab sequence idhar se suru hai or ellement ka +1 karenge or set mai check 
-// karenge agar hai tob usko count++ kardo
