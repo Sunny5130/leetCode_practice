b9105f8e3d100a92a8f03fc991a0acdd9527f3ea
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2026-01-06 19:50:38 +0530

    Time: 18 ms (3.75%) | Memory: 31.3 MB (6.95%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+    //-------------------first approach(Linear search)-----------------------//    
         // int x=0;
         // for(int i=0;i<nums.size();i++){
         //     x=x^nums[i];
         // }
         // return x;
-        int l=0,r=nums.size()-1;
-        while(l<r){
-            int mid=(l+r)/2;
-            if(mid%2!=0)mid--;
-            if(nums[mid]==nums[mid+1]){
-                l=mid+2;
-            }else{
-                r=mid;
-            }
+
+    //-------------------second approach(Binary search)-----------------------//    
+        // int l=0,r=nums.size()-1;
+        // while(l<r){
+        //     int mid=(l+r)/2;
+        //     if(mid%2!=0)mid--;
+        //     if(nums[mid]==nums[mid+1]){
+        //         l=mid+2;
+        //     }else{
+        //         r=mid;
+        //     }
+        // }
+        // return nums[l];
+    //-------------------Third approach(using extra space)-----------------------//    
+        unordered_map<int,int>map;
+        for(int i=0;i<nums.size();i++)map[nums[i]]++;
+        for(auto it=map.begin();it!=map.end();it++){
+            if(it->second==1)return it->first;
         }
-        return nums[l];
+        return 0;
     }
 };
