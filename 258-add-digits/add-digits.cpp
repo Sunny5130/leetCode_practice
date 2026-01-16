32206e4a9075b998815500e384faa2e70aff7efd
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2026-01-16 18:46:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (76.76%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    int shor(int n){
+        int sum=0;
+        while(n!=0){
+            sum+=n%10;
+            n=n/10;
+        }
+        return sum;
+    }
     int addDigits(int num) {
-        while (num >= 10) {  
-            int sum = 0;
-            while (num != 0) {
-                sum += num % 10;
-                num /= 10;
-            }
-            num = sum;
+        while(num>=10){
+            num=shor(num);
         }
         return num;
     }
