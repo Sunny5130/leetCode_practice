3e42e01b39f6653b1707d5a4d0279f6cfd8b707e
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2026-01-24 23:11:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (100.00%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        for(int i=0;i<=30;i++){
-            int digit=pow(2,i);
-            if(digit==n){
-                return true;
-            }
-        }
-        return false;
+        // for(int i=0;i<=30;i++){
+        //     int digit=pow(2,i);
+        //     if(digit==n){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        if(n<1)return false;
+        return (n & n-1)==0;
     }
 };
